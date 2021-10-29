bd1c5fa9f3404eb9a35ffaea3e99eee39a85c346
jfernando23 <47464358+jfernando23@users.noreply.github.com>
2021-10-28 19:35:08 -0500

    Ahora

--- src/main/java/Domingo_Reto3/Reto3/InterfaceReservaciones.java
+++ src/main/java/Domingo_Reto3/Reto3/InterfaceReservaciones.java
@@ -4,6 +4,9 @@
  */
 package Domingo_Reto3.Reto3;
 
+import java.util.Date;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
@@ -11,5 +14,11 @@ import org.springframework.data.repository.CrudRepository;
  * @author jefer
  */
 public interface InterfaceReservaciones extends CrudRepository<Reservaciones,Integer>{
+    public List<Reservaciones> findAllByStatus (String status); 
     
+    public List<Reservaciones> findAllByStartDateAfterAndStartDateBefore(Date dateOne, Date dateTwo);
+    
+    // SELECT clientid, COUNT(*) AS total FROM reservacion group by clientid order by desc;
+    @Query ("SELECT c.client, COUNT(c.client) from Reservaciones AS c group by c.client order by COUNT(c.client)DESC")
+    public List<Object[]> countTotalReservationsByClient();
 }
